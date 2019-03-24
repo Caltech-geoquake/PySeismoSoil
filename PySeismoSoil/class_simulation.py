c7a3d530aabbcd86cc94a4e5acf84db62436415b
jsh9 <jian.sh.7@gmail.com>
2019-03-23 22:13:04 -0700

    Add Nonlinear_Simulation class (WIP); add tests (WIP)

--- PySeismoSoil/class_simulation.py
+++ PySeismoSoil/class_simulation.py
@@ -1,9 +1,12 @@
 # Author: Jian Shi
 
 from . import helper_site_response as sr
+from . import helper_simulations as sim
 
 from .class_ground_motion import Ground_Motion
 from .class_Vs_profile import Vs_Profile
+from .class_parameters import Param_Multi_Layer
+from .class_curves import Multiple_Damping_Curves, Multiple_GGmax_Curves
 
 #%%============================================================================
 class Simulation():
@@ -12,29 +15,57 @@ class Simulation():
 
     Parameters
     ----------
-    input_motion : PySeismoSoil.class_ground_motion.Ground_Motion
+    input_motion : class_ground_motion.Ground_Motion
         Input ground motion
-    soil_profile : PySeismoSoil.class_Vs_profile.Vs_Profile
+    soil_profile : class_Vs_profile.Vs_Profile
         Soil profile
+    G_param : class_parameters.HH_Param_Multi_Layer or MKZ_Param_Multi_Layer
+        Parameters that describe the G/Gmax curves
+    xi_param : class_parameters.HH_Param_Multi_Layer or MKZ_Param_Multi_Layer
+        Parameters that describe the damping curves
+    GGmax_curves : class_curves.Multiple_GGmax_Curves
+        G/Gmax curves
+    xi_curves : class_curves.Multiple_Damping_Curves
+        Damping curves
 
     Attributes
     ----------
-    input_motion : PySeismoSoil.class_ground_motion.Ground_Motion
-        Input ground motion
-    soil_profile : PySeismoSoil.class_Vs_profile.Vs_Profile
-        Soil profile
+    Same as the inputs
     '''
-    def __init__(self, input_motion, soil_profile):
+    def __init__(self, input_motion, soil_profile, G_param=None, xi_param=None,
+                 GGmax_curves=None, xi_curves=None):
 
         if not isinstance(input_motion, Ground_Motion):
-            raise TypeError('`input_motion` need to be an object of the '
-                            '`Ground_Motion` class.')
+            raise TypeError('`input_motion` must be of class `Ground_Motion`.')
         if not isinstance(soil_profile, Vs_Profile):
-            raise TypeError('`soil_profile` need to be an object of the '
-                            '`Vs_Profile` class.')
+            raise TypeError('`soil_profile` must be of class `Vs_Profile`.')
+
+        if type(G_param) != type(xi_param):
+            raise TypeError('`G_param` and `xi_param` must be of the same type.')
+        if G_param is not None and not isinstance(G_param, Param_Multi_Layer):
+            raise TypeError('`G_param` must be of a subclass of '
+                            '`Param_Multi_Layer`, e.g., `HH_Param_Multi_Layer` '
+                            'or `MKZ_Param_Multi_Layer`.')
+        if xi_param is not None and not isinstance(xi_param, Param_Multi_Layer):
+            raise TypeError('`xi_param` must be of a subclass of '
+                            '`Param_Multi_Layer`, e.g., `HH_Param_Multi_Layer` '
+                            'or `MKZ_Param_Multi_Layer`.')
+
+        if GGmax_curves is not None and \
+        not isinstance(GGmax_curves, Multiple_GGmax_Curves):
+            raise TypeError('`GGmax_curves` must be a `Multiple_GGmax_Curves` '
+                            'object.')
+        if xi_curves is not None and \
+        not isinstance(xi_curves, Multiple_Damping_Curves):
+            raise TypeError('`xi_curves` must be a `Multiple_Damping_Curves` '
+                            'object.')
 
         self.input_motion = input_motion
         self.soil_profile = soil_profile
+        self.G_param = G_param
+        self.xi_param = xi_param
+        self.GGmax_curves = GGmax_curves
+        self.xi_curves = xi_curves
 
 #%%============================================================================
 class Linear_Simulation(Simulation):
@@ -66,3 +97,23 @@ class Linear_Simulation(Simulation):
                                        boundary=boundary, show_fig=show_fig,
                                        deconv=deconv)
         return Ground_Motion(response, 'm')  # because GM internally uses SI unit
+
+#%%============================================================================
+class Nonlinear_Simulation(Simulation):
+    '''
+    Nonlinear site response simulation
+    '''
+    def __init__(self, input_motion, soil_profile, G_param, xi_param,
+                 use_HH_model=True):
+        if G_param is None:
+            raise TypeError('`G_param` cannot be None.')
+        if xi_param is None:
+            raise TypeError('`xi_param` cannot be None.')
+        super(Nonlinear_Simulation, self).__init__(input_motion, soil_profile,
+                                                   G_param=G_param,
+                                                   xi_param=xi_param)
+        sim.check_layer_count(soil_profile, G_param, xi_param)
+
+    def run():
+        # TODO: add codes
+        pass
--- PySeismoSoil/tests/test_class_simulation.py
+++ PySeismoSoil/tests/test_class_simulation.py
@@ -2,9 +2,10 @@
 
 import unittest
 
-from PySeismoSoil.class_simulation import Linear_Simulation
+from PySeismoSoil.class_simulation import Linear_Simulation, Nonlinear_Simulation
 from PySeismoSoil.class_ground_motion import Ground_Motion
 from PySeismoSoil.class_Vs_profile import Vs_Profile
+from PySeismoSoil.class_parameters import HH_Param_Multi_Layer
 
 class Test_Class_Simulation(unittest.TestCase):
     def test_linear(self):
@@ -16,6 +17,25 @@ class Test_Class_Simulation(unittest.TestCase):
         self.assertEqual(output.accel.shape, input_motion.accel.shape)
         self.assertEqual(output.dt, input_motion.dt)
         self.assertEqual(output.npts, input_motion.npts)
+
+    def test_nonlinear_init(self):
+        input_motion = Ground_Motion('./files/sample_accel.txt', unit='m')
+        soil_profile = Vs_Profile('./files/profile_FKSH14.txt')
+        HH_G = HH_Param_Multi_Layer('./files/HH_G_FKSH14.txt')
+        HH_x = HH_Param_Multi_Layer('./files/HH_X_FKSH14.txt')
+
+        # this should succeed
+        Nonlinear_Simulation(input_motion, soil_profile, HH_G, HH_x)
+
+        # this should fail with ValueError
+        HH_G_data = HH_G.param_list
+        HH_x_data = HH_x.param_list
+        HH_G_ = HH_Param_Multi_Layer(HH_G_data[:-1])  # exclude one layer
+        HH_x_ = HH_Param_Multi_Layer(HH_x_data[:-1])  # exclude one layer
+        with self.assertRaisesRegex(ValueError, 'Not enough sets of parameters'):
+            Nonlinear_Simulation(input_motion, soil_profile, HH_G_, HH_x)
+        with self.assertRaisesRegex(ValueError, 'Not enough sets of parameters'):
+            Nonlinear_Simulation(input_motion, soil_profile, HH_G, HH_x_)
 
 if __name__ == '__main__':
     SUITE = unittest.TestLoader().loadTestsFromTestCase(Test_Class_Simulation)
