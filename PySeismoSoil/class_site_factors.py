d24d28bb1e944d9cca6d2d56724324a08b3df182
jsh9 <25124332+jsh9@users.noreply.github.com>
2019-03-28 17:50:45 -0700

    Rename "z1000" into "z1" in class_site_factors.py

--- PySeismoSoil/class_site_factors.py
+++ PySeismoSoil/class_site_factors.py
@@ -18,36 +18,36 @@ class Site_Factors():
     ----------
     Vs30_in_meter_per_sec : float
         Vs30 values in SI unit
-    z1000_in_m : float
-        z1000 (basin depth) in meters
+    z1_in_m : float
+        z1 (basin depth) in meters
     PGA_in_g : float
         PGA in g
     lenient : bool
-        Whether to ensure the given Vs30, z1000, and PGA values are within the
+        Whether to ensure the given Vs30, z1, and PGA values are within the
         valid range. If False and the given values fall outside the valid
         range, the given values (e.g., Vs30 = 170 m/s) will be treated as
         the closest boundary values (e.g., Vs30 = 175 m/s).
 
     Attributes
     ----------
-    Vs30, z1000, PGA : float
+    Vs30, z1, PGA : float
         Same as the inputs (same values, same unit)
     '''
 
     Vs30_array = [175, 200, 250, 300, 350, 400, 450, 500, 550, 600,
                   650, 700, 750, 800, 850, 900, 950]
-    z1000_array = [8, 16, 24, 36, 75, 150, 300, 450, 600, 750, 900]
+    z1_array = [8, 16, 24, 36, 75, 150, 300, 450, 600, 750, 900]
     PGA_array = [0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.75, 1.0, 1.25, 1.5]
 
     #%%------------------------------------------------------------------------
-    def __init__(self, Vs30_in_meter_per_sec, z1000_in_m, PGA_in_g,
+    def __init__(self, Vs30_in_meter_per_sec, z1_in_m, PGA_in_g,
                  lenient=False):
         self.dir_amplif = pkg_resources.resource_filename('PySeismoSoil',
                                                           'data/amplification/')
         self.dir_phase = pkg_resources.resource_filename('PySeismoSoil',
                                                          'data/phase/')
 
-        status = Site_Factors._range_check(Vs30_in_meter_per_sec, z1000_in_m,
+        status = Site_Factors._range_check(Vs30_in_meter_per_sec, z1_in_m,
                                            PGA_in_g)
         if 1 in status:
             if not lenient:
@@ -56,19 +56,19 @@ class Site_Factors():
                 Vs30_in_meter_per_sec = 175 if Vs30_in_meter_per_sec < 175 else 950
         if 2 in status:
             if not lenient:
-                raise ValueError('z1000_in_m should be between [8, 900] m')
+                raise ValueError('z1_in_m should be between [8, 900] m')
             else:
-                z1000_in_m = 8 if z1000_in_m < 8 else 1000
+                z1_in_m = 8 if z1_in_m < 8 else 1000
         if 3 in status:
             if not lenient:
                 raise ValueError('PGA should be between [0.01g, 1.5g]')
             else:
                 PGA_in_g = 0.01 if PGA_in_g < 0.01 else 1.5
         if 4 in status:
-            raise ValueError('Vs30 and z1000 combination not valid')
+            raise ValueError('Vs30 and z1 combination not valid')
 
         self.Vs30 = Vs30_in_meter_per_sec
-        self.z1000 = z1000_in_m
+        self.z1 = z1_in_m
         self.PGA = PGA_in_g
 
     #%%------------------------------------------------------------------------
@@ -196,41 +196,41 @@ class Site_Factors():
             Amplification or phase shift, interpolated
         '''
         Vs30 = self.Vs30
-        z1000 = self.z1000
+        z1 = self.z1
         PGA = self.PGA
 
         combinations = self._locate_grids()
 
-        points = []  # to hold reference (Vs30, z1000, PGA) points
+        points = []  # to hold reference (Vs30, z1, PGA) points
         y_list = []  # to hold values at these reference points
-        for Vs30_i, z1000_i, PGA_i in combinations:
+        for Vs30_i, z1_i, PGA_i in combinations:
             Vs30_grid = Site_Factors.Vs30_array[Vs30_i]
-            z1000_grid = Site_Factors.z1000_array[z1000_i]
+            z1_grid = Site_Factors.z1_array[z1_i]
             PGA_grid = Site_Factors.PGA_array[PGA_i]
             x, y  = Site_Factors._query(amplif_or_phase,
-                                        Vs30_grid, z1000_grid, PGA_grid,
+                                        Vs30_grid, z1_grid, PGA_grid,
                                         method=method, Fourier=Fourier,
                                         data_dir=data_dir)
-            points.append((Vs30_grid, z1000_grid, PGA_grid))
+            points.append((Vs30_grid, z1_grid, PGA_grid))
             y_list.append(y)
 
         y_interp \
-            = Site_Factors._interpolate(points, y_list, (Vs30, z1000, PGA))
+            = Site_Factors._interpolate(points, y_list, (Vs30, z1, PGA))
 
         if show_interp_plots:
-            Site_Factors._plot_interp(points, (Vs30, z1000, PGA),
+            Site_Factors._plot_interp(points, (Vs30, z1, PGA),
                                       x, y_list, y_interp, Fourier=Fourier)
 
         return x, y_interp
 
     #%%------------------------------------------------------------------------
     @staticmethod
-    def _query(amplif_or_phase, Vs30, z1000, PGA, Fourier=True,
+    def _query(amplif_or_phase, Vs30, z1, PGA, Fourier=True,
                method='nl_hh', data_dir=None):
         '''
         Query amplification or phase factors from pre-computed .csv files. The
-        given Vs30, z1000_in_m, and PGA_in_g values need to match the
-        pre-defined values (see `Vs30_array`, `z1000_array`, and `PGA_array`
+        given Vs30, z1_in_m, and PGA_in_g values need to match the
+        pre-defined values (see `Vs30_array`, `z1_array`, and `PGA_array`
         at the top of this file).
 
         Parameters
@@ -239,7 +239,7 @@ class Site_Factors():
             Specifies what to query: amplification or phase
         Vs30 : scalar
             Vs30 value. Unit: m/s
-        z1000 : scalar
+        z1 : scalar
             Basin depth (i.e., depth to Vs = 1000 m/s). Unit: m
         PGA : scalar
             Peak ground acceleration. Unit: g
@@ -264,8 +264,8 @@ class Site_Factors():
 
         if Vs30 not in Site_Factors.Vs30_array:
             raise ValueError('`Vs30` should be in %s.' % Site_Factors.Vs30_array)
-        if z1000 not in Site_Factors.z1000_array:
-            raise ValueError('`z1000` should be in %s.' % Site_Factors.z1000_array)
+        if z1 not in Site_Factors.z1_array:
+            raise ValueError('`z1` should be in %s.' % Site_Factors.z1_array)
         if PGA not in Site_Factors.PGA_array:
             raise ValueError('`PGA` should be in %s.' % Site_Factors.PGA_array)
 
@@ -274,14 +274,14 @@ class Site_Factors():
 
         if amplif_or_phase == 'amplif':
             if Fourier:
-                y_filename = '%d_%03d_af_fs_%s_avg.csv' % (Vs30, z1000, method)
-                x_filename = '%d_%03d_freq.csv' % (Vs30, z1000)
+                y_filename = '%d_%03d_af_fs_%s_avg.csv' % (Vs30, z1, method)
+                x_filename = '%d_%03d_freq.csv' % (Vs30, z1)
             else:  # response spectra
-                y_filename = '%d_%03d_af_rs_%s_avg.csv' % (Vs30, z1000, method)
-                x_filename = '%d_%03d_period.csv' % (Vs30, z1000)
+                y_filename = '%d_%03d_af_rs_%s_avg.csv' % (Vs30, z1, method)
+                x_filename = '%d_%03d_period.csv' % (Vs30, z1)
         else:  # phase shift
-            y_filename = '%d_%03d_phase_shift_%s_avg.csv' % (Vs30, z1000, method)
-            x_filename = '%d_%03d_freq.csv' % (Vs30, z1000)
+            y_filename = '%d_%03d_phase_shift_%s_avg.csv' % (Vs30, z1, method)
+            x_filename = '%d_%03d_freq.csv' % (Vs30, z1)
 
         y = np.genfromtxt(os.path.join(data_dir, y_filename), delimiter=',')
         x = np.genfromtxt(os.path.join(data_dir, x_filename), delimiter=',')
@@ -293,35 +293,35 @@ class Site_Factors():
     #%%------------------------------------------------------------------------
     def _locate_grids(self):
         '''
-        Locates the "reference grids", i.e., rereference Vs30, z1000, and PGA
+        Locates the "reference grids", i.e., rereference Vs30, z1, and PGA
         values (in terms of the indices, not actual values).
 
-        Returns all possible combinations of Vs30, z1000, and PGA values.
+        Returns all possible combinations of Vs30, z1, and PGA values.
         '''
-        Vs30_loc, z1000_loc, PGA_loc \
-            = Site_Factors._find_neighbors(self.Vs30, self.z1000, self.PGA)
+        Vs30_loc, z1_loc, PGA_loc \
+            = Site_Factors._find_neighbors(self.Vs30, self.z1, self.PGA)
 
-        combinations = list(itertools.product(Vs30_loc, z1000_loc, PGA_loc))
+        combinations = list(itertools.product(Vs30_loc, z1_loc, PGA_loc))
         assert(len(list(combinations)) == 8)
 
         return combinations
 
     #%%------------------------------------------------------------------------
     @staticmethod
-    def _find_neighbors(Vs30_in_mps, z1000_in_m, PGA_in_g):
+    def _find_neighbors(Vs30_in_mps, z1_in_m, PGA_in_g):
         '''
-        Find the indices of Vs30, z1000, and PGA that surround the provided values.
-        If the provided values fall onto the "reference" Vs30, z1000, or PGA values,
+        Find the indices of Vs30, z1, and PGA that surround the provided values.
+        If the provided values fall onto the "reference" Vs30, z1, or PGA values,
         two indices are still returned.
 
         The three inputs need to already within the correct range.
         '''
 
         Vs30_loc = Site_Factors._search_sorted(Vs30_in_mps, Site_Factors.Vs30_array)
-        z1000_loc = Site_Factors._search_sorted(z1000_in_m, Site_Factors.z1000_array)
+        z1_loc = Site_Factors._search_sorted(z1_in_m, Site_Factors.z1_array)
         PGA_loc = Site_Factors._search_sorted(PGA_in_g, Site_Factors.PGA_array)
 
-        return Vs30_loc, z1000_loc, PGA_loc
+        return Vs30_loc, z1_loc, PGA_loc
 
     #%%------------------------------------------------------------------------
     @staticmethod
@@ -418,14 +418,14 @@ class Site_Factors():
                      phases=None, phase_interp=None, Fourier=True):
         '''
         Show a plot of the amplification and/or phase shift factors at the
-        reference (Vs30, z1000, PGA) points, as well as the interpolated factors.
+        reference (Vs30, z1, PGA) points, as well as the interpolated factors.
 
         Parameters
         ----------
         ref_points : list of tuples
-            List of tuples of (Vs30, z1000, PGA), which are the reference points
+            List of tuples of (Vs30, z1, PGA), which are the reference points
         query_point : tuple
-            A tuple of (Vs30, z1000, PGA) at which you want to query the factors
+            A tuple of (Vs30, z1, PGA) at which you want to query the factors
         T_or_freq : numpy.ndarray
             Period or frequency array
         amps : list of numpy.ndarray
@@ -507,22 +507,22 @@ class Site_Factors():
 
     #%%------------------------------------------------------------------------
     @staticmethod
-    def _range_check(Vs30_in_mps, z1000_in_m, PGA_in_g):
+    def _range_check(Vs30_in_mps, z1_in_m, PGA_in_g):
         '''
-        Check if the provided Vs30, z1000_in_m, and PGA_in_g values are within
+        Check if the provided Vs30, z1_in_m, and PGA_in_g values are within
         the pre-computed range.
 
         Meanings of different return values:
         1 - Vs30 outside of range [175, 950] m/s
-        2 - z1000_in_m outside of range [8, 900] m
+        2 - z1_in_m outside of range [8, 900] m
         3 - PGA outside of range [0.01g, 1.5g]
-        4 - Vs30 and z1000 combination is not valid
+        4 - Vs30 and z1 combination is not valid
         '''
 
         if not isinstance(Vs30_in_mps, (float, int, np.number)):
             raise TypeError('Vs30 must be int, float, or numpy.number.')
-        if not isinstance(z1000_in_m, (float, int, np.number)):
-            raise TypeError('z1000_in_m must be int, float, or numpy.number.')
+        if not isinstance(z1_in_m, (float, int, np.number)):
+            raise TypeError('z1_in_m must be int, float, or numpy.number.')
         if not isinstance(PGA_in_g, (float, int, np.number)):
             raise TypeError('PGA_in_g must be int, float, or numpy.number.')
 
@@ -531,27 +531,27 @@ class Site_Factors():
         if Vs30_in_mps < 175 or Vs30_in_mps > 950:
             status.append(1)
             return status
-        if z1000_in_m < 8 or z1000_in_m > 900:
+        if z1_in_m < 8 or z1_in_m > 900:
             status.append(2)
             return status
         if PGA_in_g < 0.01 or PGA_in_g > 1.5:
             status.append(3)
 
-        if Vs30_in_mps > 400 and z1000_in_m > 750:
+        if Vs30_in_mps > 400 and z1_in_m > 750:
             status.append(4)
-        elif Vs30_in_mps > 450 and z1000_in_m > 600:
+        elif Vs30_in_mps > 450 and z1_in_m > 600:
             status.append(4)
-        elif Vs30_in_mps > 550 and z1000_in_m > 450:
+        elif Vs30_in_mps > 550 and z1_in_m > 450:
             status.append(4)
-        elif Vs30_in_mps > 600 and z1000_in_m > 300:
+        elif Vs30_in_mps > 600 and z1_in_m > 300:
             status.append(4)
-        elif Vs30_in_mps > 650 and z1000_in_m > 150:
+        elif Vs30_in_mps > 650 and z1_in_m > 150:
             status.append(4)
-        elif Vs30_in_mps > 750 and z1000_in_m > 75:
+        elif Vs30_in_mps > 750 and z1_in_m > 75:
             status.append(4)
-        elif Vs30_in_mps > 800 and z1000_in_m > 36:
+        elif Vs30_in_mps > 800 and z1_in_m > 36:
             status.append(4)
-        elif Vs30_in_mps > 850 and z1000_in_m > 16:
+        elif Vs30_in_mps > 850 and z1_in_m > 16:
             status.append(4)
         else:
             pass
