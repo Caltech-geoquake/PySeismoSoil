aa10e7514b7936134d9b70061784ceee2b90ccda
jsh9 <jian.sh.7@gmail.com>
2019-05-26 18:20:09 -0700

    Update transfer/amplif functions in `Vs_Profile`; other smaller changes

--- PySeismoSoil/tests/test_class_simulation_results.py
+++ PySeismoSoil/tests/test_class_simulation_results.py
@@ -5,7 +5,6 @@ import numpy as np
 
 from PySeismoSoil.class_ground_motion import Ground_Motion
 from PySeismoSoil.class_Vs_profile import Vs_Profile
-from PySeismoSoil.class_frequency_spectrum import Frequency_Spectrum
 from PySeismoSoil.class_simulation_results import Simulation_Results
 
 import PySeismoSoil.helper_site_response as sr
@@ -28,10 +27,9 @@ class Test_Class_Simulation_Results(unittest.TestCase):
         max_gamma_tau = np.column_stack((depth_midpoint,
                                          depth_midpoint * 1,
                                          depth_midpoint * 2))
-        freq, tf_ro = vs_profile.transfer_function()[:2]
-        trans_func = Frequency_Spectrum(np.column_stack((freq, tf_ro)))
+        tf_RO, _, _ = vs_profile.get_transfer_function()
         sim_results = Simulation_Results(accel_in, accel_out, vs_profile,
-                                         max_a_v_d, max_gamma_tau, trans_func)
+                                         max_a_v_d, max_gamma_tau, tf_RO)
         sim_results.plot(save_fig=False)
 
 if __name__ == '__main__':
