0ce4073c9dfccf770c231735af80b98f69760392
jsh9 <jian.sh.7@gmail.com>
2019-05-31 13:40:39 -0700

    Add a new class: `Damping_Calibration`; add tests

---
+++ PySeismoSoil/class_damping_calibration.py
@@ -0,0 +1,124 @@
+# Author: Jian Shi
+
+import numpy as np
+
+from . import helper_hh_calibration as hhc
+
+from .class_Vs_profile import Vs_Profile
+from .class_parameters import HH_Param_Multi_Layer
+from .class_curves import Damping_Curve, Multiple_Damping_Curves
+
+class Damping_Calibration():
+    '''
+    A class to generate damping curves (and associated soil model parameters)
+    from a given Vs profile.
+    '''
+    def __init__(self, vs_profile):
+        if not isinstance(vs_profile, Vs_Profile):
+            raise TypeError('`vs_profile` must be of type Vs_Profile.')
+        self.vs_profile = vs_profile
+
+    def get_damping_curves(self, strain_in_pct=np.logspace(-2, 1),
+                           use_Darendeli_Dmin=False, show_fig=False):
+        '''
+        Calculate damping curves using empirical formulas by Darendeli (2001).
+
+        Parameters
+        ----------
+        strain_in_pct : numpy.ndarray
+            Strain array. Must be a 1D numpy array. Unit: %
+        use_Darendeli_Dmin : bool
+            If ``True``, use the D_min values estimated by the empirical
+            formulas in Darendeli (2001) as the small-strain damping ratios
+            of each soil layer. If ``False``, use the damping ratio provided
+            in ``vs_profile`` (or estimated using PySeismoSoil's built-in
+            Vs-damping correlation).
+        show_fig : bool
+            Whether to show a figure of the damping curves of each layer.
+
+        Returns
+        -------
+        mdc : PySeismoSoil.class_curves.Multiple_Damping_Curves
+            Damping curves for all the soil layers (i.e., not including the
+            rock halfspace at the bottom).
+        '''
+        h = self.vs_profile.vs_profile[:-1, 0]
+        Vs = self.vs_profile.vs_profile[:-1, 1]
+        n_layer = len(Vs)
+
+        if self.vs_profile.vs_profile.shape[1] == 5:  # there can only be 5 or 2 columns
+            rho = self.vs_profile.vs_profile[:-1, 3]
+        else:  # only 2 columns
+            rho = hhc._calc_rho(h, Vs)
+
+        sigma_v0 = hhc._calc_vertical_stress(h, rho)
+        OCR = hhc._calc_OCR(Vs, rho, sigma_v0)
+        PI = hhc._calc_PI(Vs)
+        phi = 30
+        _, xi, _ = hhc.produce_Darendeli_curves(sigma_v0, PI, OCR=OCR,
+                                                K0=None, phi=phi,
+                                                strain_in_pct=strain_in_pct)
+        assert(xi.shape[1] == n_layer)
+
+        curve_list = []
+        for j in range(n_layer):
+            xi_j = xi[:, j]
+            if not use_Darendeli_Dmin:
+                xi_j -= xi_j[0]
+                xi_j += self.vs_profile.vs_profile[j, 2]
+            dc = Damping_Curve(np.column_stack((strain_in_pct, xi_j)),
+                               strain_unit='%', damping_unit='1',
+                               interpolate=False, check_values=True)
+            curve_list.append(dc)
+        mdc = Multiple_Damping_Curves(curve_list)
+
+        if show_fig:
+            mdc.plot()
+
+        return mdc
+
+    def get_HH_x_param(self, **kwargs):
+        '''
+        Obtain HH_x parameters for each layer (i.e., HH model parameters
+        that best fit given damping values, for each layer).
+
+        Parameters
+        ----------
+        kwargs :
+            Keyword arguments to be passed to this method:
+                PySeismoSoil.class_curves.Multiple_Damping_Curves.get_all_HH_x_params().
+            Check its documentation for details:
+                https://pyseismosoil.readthedocs.io/en/stable/api_docs/class_curves.html#PySeismoSoil.class_curves.Multiple_Damping_Curves.get_all_HH_x_params
+
+        Returns
+        -------
+        HH_x_param : PySeismoSoil.class_parameters.HH_Param_Multi_Layer
+            The best parameters for each soil layer found in the optimization.
+        '''
+        mdc = self.get_damping_curves(strain_in_pct=np.geomspace(0.001, 15, 100),
+                                      show_fig=False)
+        HH_x_param = mdc.get_all_HH_x_params(**kwargs)
+        return HH_x_param
+
+    def get_H4_x_param(self, **kwargs):
+        '''
+        Obtain H4_x parameters for each layer (i.e., MKZ model parameters
+        that best fit given damping values, for each layer).
+
+        Parameters
+        ----------
+        kwargs :
+            Keyword arguments to be passed to this method:
+                PySeismoSoil.class_curves.Multiple_Damping_Curves.get_all_H4_x_params().
+            Check its documentation for details:
+                https://pyseismosoil.readthedocs.io/en/stable/api_docs/class_curves.html#PySeismoSoil.class_curves.Multiple_Damping_Curves.get_all_H4_x_params
+
+        Returns
+        -------
+        H4_x_param : PySeismoSoil.class_parameters.H4_Param_Multi_Layer
+            The best parameters for each soil layer found in the optimization.
+        '''
+        mdc = self.get_damping_curves(strain_in_pct=np.geomspace(0.001, 15, 100),
+                                      show_fig=False)
+        H4_x_param = mdc.get_all_HH_x_params(**kwargs)
+        return H4_x_param
---
+++ PySeismoSoil/tests/test_class_damping_calibration.py
@@ -0,0 +1,67 @@
+# Author: Jian Shi
+
+import unittest
+import numpy as np
+
+from PySeismoSoil.class_Vs_profile import Vs_Profile
+from PySeismoSoil.class_damping_calibration import Damping_Calibration
+
+class Test_Class_Damping_Calibration(unittest.TestCase):
+    def test_init(self):
+        filename = './files/profile_FKSH14.txt'
+        vs_profile_array = np.genfromtxt(filename)
+
+        # Case 1: incorrect input type
+        with self.assertRaisesRegex(TypeError, 'must be of type Vs_Profile'):
+            Damping_Calibration(vs_profile_array)
+
+        # Case 2: correct input type
+        vs_profile = Vs_Profile(filename)
+        Damping_Calibration(vs_profile)
+
+    def test_get_damping_curves(self):
+        vs_profile = Vs_Profile('./files/profile_FKSH14.txt')
+        d_cal = Damping_Calibration(vs_profile)
+        strain_in_pct = np.logspace(-2, 1, num=23)
+        mdc = d_cal.get_damping_curves(strain_in_pct=strain_in_pct,
+                                       use_Darendeli_Dmin=True, show_fig=True)
+        self.assertEqual(mdc.n_layer, vs_profile.n_layer)
+
+        # Check two things:
+        #  (1) The unit of strains are correct (i.e., %), and no interpolation happened
+        #  (2) The unit of the damping ratios are correct (i.e., %)
+        curve_matrix = mdc.get_curve_matrix()
+        for i in range(mdc.n_layer):
+            gamma = curve_matrix[:, i * 4 + 2]
+            xi = curve_matrix[:, i * 4 + 3]
+            self.assertTrue(np.allclose(gamma, strain_in_pct, atol=1e-5))
+            self.assertGreaterEqual(xi[-1], 1.0)  # last element of each damping curve
+
+        # Check `use_Darendeli_Dmin` operates as expected
+        mdc_ = d_cal.get_damping_curves(strain_in_pct=strain_in_pct,
+                                        use_Darendeli_Dmin=False, show_fig=True)
+        curve_matrix_ = mdc_.get_curve_matrix()
+        for i in range(mdc_.n_layer):
+            xi = curve_matrix[:, i * 4 + 3]  # damping with Darendeli's D_min
+            xi_ = curve_matrix_[:, i * 4 + 3]  # damping specified in `vs_profile`
+            self.assertTrue(np.allclose(xi_[0], vs_profile.vs_profile[i, 2] * 100,
+                                        atol=1e-5))
+            self.assertTrue(np.allclose(xi_ - xi_[0], xi - xi[0]))
+
+    def test_get_HH_x_param(self):
+        vs_profile = Vs_Profile('./files/profile_FKSH14.txt')
+        d_cal = Damping_Calibration(vs_profile)
+        # Only test that `get_HH_x_param()` can run without bugs
+        d_cal.get_HH_x_param(pop_size=1, n_gen=1, save_txt=False, use_scipy=True,
+                             show_fig=True)
+
+    def test_get_H4_x_param(self):
+        vs_profile = Vs_Profile('./files/profile_FKSH14.txt')
+        d_cal = Damping_Calibration(vs_profile)
+        # Only test that `get_H4_x_param()` can run without bugs
+        d_cal.get_H4_x_param(pop_size=1, n_gen=1, save_txt=False, use_scipy=True,
+                             show_fig=True)
+
+if __name__ == '__main__':
+    SUITE = unittest.TestLoader().loadTestsFromTestCase(Test_Class_Damping_Calibration)
+    unittest.TextTestRunner(verbosity=2).run(SUITE)
