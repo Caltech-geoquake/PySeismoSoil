f1492855e2229b4dd5ee952df0c741e01519a253
jsh9 <jian.sh.7@gmail.com>
2019-06-02 16:08:45 -0700

    Update version number to 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='PySeismoSoil',
-    version='0.0.1',
+    version='0.1.0',
     description='PySeismoSoil',
     author='Jian Shi',
     license='GPL v3.0',
