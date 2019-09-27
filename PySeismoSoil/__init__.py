6726bf23d3334979647954514f4b99643e8cdac1
jsh9 <jian.sh.7@gmail.com>
2019-09-27 00:45:56 -0700

    Fix a bug related to z1 out of range issue

--- PySeismoSoil/__init__.py
+++ PySeismoSoil/__init__.py
@@ -1,3 +1,3 @@
 # Author: Jian Shi
 
-__version__ = '0.2.6'
+__version__ = 'v0.2.7'
--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2019, California Institute of Technology'
 author = 'Jian Shi'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.6'
+release = 'v0.2.7'
 
 
 # -- General configuration ---------------------------------------------------
