0a03c8a6c8ff61b5bba7709a6ec090aee6220aa1
jsh9 <jian.sh.7@gmail.com>
2019-11-05 23:46:45 -0800

    v0.3.0 v0.3.1

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2019, California Institute of Technology'
 author = 'Jian Shi'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.3.0'
+release = 'v0.3.1'
 
 
 # -- General configuration ---------------------------------------------------
