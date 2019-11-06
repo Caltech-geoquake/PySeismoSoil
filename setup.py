2dfa9bb583c1b6def9edd3e078f3e0316a3cdad4
jsh9 <25124332+jsh9@users.noreply.github.com>
2019-11-05 23:46:45 -0800

    v0.3.0 v0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='PySeismoSoil',
-    version='v0.3.0',
+    version='v0.3.1',
     description='PySeismoSoil',
     author='Jian Shi',
     license='BSD 3',
