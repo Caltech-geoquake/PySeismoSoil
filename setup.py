6f24e4ae6bcb7e6161e447fbce26e64a4c6838b0
jsh9 <25124332+jsh9@users.noreply.github.com>
2022-06-28 00:41:25 -0700

    Move dependencies to requirements.txt

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from setuptools import setup
 
+
+def load_requirements():
+    with open('requirements.txt') as fp:
+        requirements = fp.readlines()
+    # END
+    return [_.strip() for _ in requirements]
+
 setup(
     name='PySeismoSoil',
     version='v0.4.1',
@@ -8,17 +15,14 @@ setup(
     license='BSD 3',
     url='https://github.com/jsh9/PySeismoSoil',
     packages=['PySeismoSoil'],
-    classifiers=['Intended Audience :: Science/Research',
-                'Topic :: Scientific/Engineering',
-                'Programming Language :: Python :: 3.5',
-                'Programming Language :: Python :: 3.6',
-                'Programming Language :: Python :: 3.7',
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
-    install_requires=['numpy>=1.11.0',
-                      'matplotlib>=2.0.0',
-                      'scipy>=1.1.0',
-                      'numba>=0.38.0'
-    ],
+    install_requires=load_requirements(),
     python_requires='>=3.6',
     include_package_data=True,
 )
