ba07c5a1f3e25a3d7d98dbef42a9370a2eae1598
jsh9 <25124332+jsh9@users.noreply.github.com>
2022-06-29 23:33:05 -0700

    Stop ignoring E117

--- run_linting.sh
+++ run_linting.sh
@@ -1,1 +1,1 @@
-flake8 ./PySeismoSoil --count --statistics --ignore=C901,E203,E741,E221,W605,E502,E116,W504,E266,E114,E117,E222 --max-line-length=99 --max-complexity=10
+flake8 ./PySeismoSoil --count --statistics --ignore=C901,E203,E741,E221,W605,E502,E116,W504,E266,E114,E222 --max-line-length=99 --max-complexity=10
