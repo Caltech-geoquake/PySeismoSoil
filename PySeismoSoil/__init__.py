85fce24f48f79179fbf42eaec358937b09038184
jsh9 <25124332+jsh9@users.noreply.github.com>
2022-07-04 15:05:07 -0700

    v0.4.4 -> 0.4.5

--- PySeismoSoil/__init__.py
+++ PySeismoSoil/__init__.py
@@ -1,3 +1,3 @@
 # Author: Jian Shi
 
-__version__ = 'v0.4.4'
+__version__ = 'v0.4.5'
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='PySeismoSoil',
-    version='v0.4.4',
+    version='v0.4.5',
     description='PySeismoSoil',
     author='Jian Shi',
     license='BSD 3',
