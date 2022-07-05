b568784afbe668f0a13939c1884c0c7786486914
jsh9 <25124332+jsh9@users.noreply.github.com>
2022-07-04 21:49:50 -0700

    Fix typos

--- tests/test_class_svm.py
+++ tests/test_class_svm.py
@@ -47,8 +47,8 @@ class Test_Class_SVM(unittest.TestCase):
         discr_profile = svm.get_discretized_profile(Vs_increment=100, show_fig=False)
         self.assertTrue(isinstance(discr_profile, Vs_Profile))
         if svm.has_bedrock_Vs:  # bedrock Vs must match
-            self.assertTrue(svm.bedrock_Vs, discr_profile.vs_profile[-1, 1])
-            self.assertTrue(0, discr_profile.vs_profile[-1, 0])
+            self.assertEqual(svm.bedrock_Vs, discr_profile.vs_profile[-1, 1])
+            self.assertEqual(0, discr_profile.vs_profile[-1, 0])
 
     def test_get_discretized_profile__invalid_Vs_increment(self):
         svm = SVM(target_Vs30=256, z1=100, show_fig=False)
