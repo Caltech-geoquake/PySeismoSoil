fd5f874b8e4e28294a512becbdc7d7fde3149aca
jsh9 <25124332+jsh9@users.noreply.github.com>
2022-09-25 23:34:52 -0700

    Set up style linting config

--- run_linting.sh
+++ run_linting.sh
@@ -1,2 +1,5 @@
-pip install flake8
-flake8 ./PySeismoSoil --count --statistics --ignore=C901,E203,E741,E221,W605,E502,E116,W504,E266,E114,E222 --max-line-length=99 --max-complexity=10
+pip install flake8 wemake-python-styleguide flake8-mutable flake8-newspaper-style flake8-length
+echo ""
+echo "******************************"
+echo ""
+flake8 ./PySeismoSoil
