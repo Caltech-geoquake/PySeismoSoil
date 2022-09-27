03b224f57f38abe10c2288f3634fa00578f0d355
jsh9 <25124332+jsh9@users.noreply.github.com>
2022-09-27 00:00:03 -0700

    Add  flake8-newspaper-style

--- run_linting.sh
+++ run_linting.sh
@@ -1,7 +1,10 @@
-pip install flake8 wemake-python-styleguide flake8-commas flake8-mutable flake8-length flake8-absolute-import
+pip install flake8 wemake-python-styleguide flake8-commas flake8-mutable flake8-length flake8-absolute-import flake8-newspaper-style
 echo ""
 echo "******************************"
 echo ""
-#flake8 --ignore=D101,D102,N801 ./tests/*.py
+flake8 --select NEWS100 PySeismoSoil/
+# flake8 --config=setup.cfg PySeismoSoil/
 echo "----------------------"
-flake8 --select WPS317 ./tests/*.py
+# flake8 --config=setup.cfg --ignore=D101,D102,N801 tests/
+#echo "----------------------"
+#flake8 --select WPS317 ./tests/*.py
