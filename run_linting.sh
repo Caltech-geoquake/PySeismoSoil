63aee14c38428aa14570f52f9dc07039c773c506
jsh9 <25124332+jsh9@users.noreply.github.com>
2022-09-26 22:06:27 -0700

    Update linting script

--- run_linting.sh
+++ run_linting.sh
@@ -2,6 +2,6 @@ pip install flake8 wemake-python-styleguide flake8-commas flake8-mutable flake8-
 echo ""
 echo "******************************"
 echo ""
-flake8 ./PySeismoSoil/helper_site_response.py
+flake8 ./PySeismoSoil/*.py
 echo "----------------------"
-flake8 --select WPS317 ./PySeismoSoil/helper_site_response.py
+flake8 --select WPS317 ./PySeismoSoil/*.py
