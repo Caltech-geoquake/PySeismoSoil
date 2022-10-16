f78f567b2643ee842d1ab7bbd207b83b9282fab7
jsh9 <25124332+jsh9@users.noreply.github.com>
2022-10-15 21:18:18 -0700

    Use setup.cfg

--- setup.py
+++ setup.py
@@ -1,39 +1,4 @@
-from pathlib import Path
 from setuptools import setup
 
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
 
-
-def load_requirements(req_filename):
-    # Reading dependencies from requirements.txt avoid readthedocs.org from
-    # installing non-stable dependencies (e.g., "release candidate" versions)
-    with open(this_directory / Path(req_filename)) as fp:
-        requirements = fp.readlines()
-    # END
-    return [_.strip() for _ in requirements]
-
-
-setup(
-    name='PySeismoSoil',
-    version='v0.4.9',
-    description='PySeismoSoil',
-    long_description=long_description,  # will get passed to PyPI.org
-    long_description_content_type="text/markdown",  # otherwise the default is RST
-    author='Jian Shi',
-    license='BSD 3',
-    url='https://github.com/jsh9/PySeismoSoil',
-    packages=['PySeismoSoil'],
-    classifiers=[
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-    ],
-    install_requires=load_requirements('requirements.txt'),
-    python_requires='>=3.6',
-    include_package_data=True,
-)
+setup()
--- update_version.sh
+++ update_version.sh
@@ -1,6 +1,7 @@
 echo "Updating version number in files: from $1 to $2"
 sed -i "" -e "s/$1/$2/g" README.md
 sed -i "" -e "s/$1/$2/g" setup.py
+sed -i "" -e "s/$1/$2/g" setup.cfg
 sed -i "" -e "s/$1/$2/g" ./docs/source/index.rst
 sed -i "" -e "s/$1/$2/g" ./docs/source/conf.py
 sed -i "" -e "s/$1/$2/g" ./PySeismoSoil/__init__.py
