991d9d1b6f122b60b054547522d1a805813fce6e
Flora Xia <45961129+xia-fr@users.noreply.github.com>
2024-03-05 19:36:56 -0800

    Added batch score calculation

--- PySeismoSoil/class_gof_scores.py
+++ PySeismoSoil/class_gof_scores.py
@@ -1,5 +1,7 @@
 import numpy as np
-from matplotlib import pyplot as plt
+import itertools
+import multiprocessing as mp
+from typing import Any
 
 from PySeismoSoil import helper_generic as hlp
 from PySeismoSoil import helper_gof_scores as gof
@@ -50,11 +52,9 @@ class GOF_Scores:
         self.simulation = simulation
 
         self.scores = np.full((10), None)
-
-    # TODO: GOF.D_10() FUNCTION
         
-    def __repr__(self) -> str:
-        """Define a representation of calculated scores."""
+    def __str__(self) -> str:
+        """Define a string representation of calculated scores."""
 
         sn = ['Normalized Arias Intensity (S1)', 
                   'Normalized Energy Integral (S2)',
@@ -82,6 +82,18 @@ class GOF_Scores:
 
         return text
     
+    def get_meas(self) -> np.ndarray:
+        """
+        Returns two-column measurement array, where the first column is time.
+        """
+        return self.measurement
+    
+    def get_simu(self) -> np.ndarray:
+        """
+        Returns two-column simulation array, where the first column is time.
+        """
+        return self.simulation
+    
     def get_scores(self) -> np.ndarray:
         """
         Returns entire score array, with 'None' for scores that haven't been calculated.
@@ -93,8 +105,11 @@ class GOF_Scores:
         fmin: float | None = None,
         fmax: float | None = None,
         *,
-        score_groups: tuple[bool, bool, bool, bool] = (True, True, True, False),
-        verbose: bool = True,
+        score_arias: bool = True,
+        score_rms: bool = True,
+        score_spectra: bool = True,
+        score_cross_correl: bool = False,
+        verbose: bool = False,
         show_fig: bool = False,
     ) -> np.ndarray:
         """
@@ -117,6 +132,8 @@ class GOF_Scores:
         scores : np.ndarray
             A vector containing the goodness-of-fit scores, in the following order:
             [d1, d2, d3, d4, d5, d6, d7, d8, d9, d10]
+            If the score hasn't been calculated, it will be omitted from the
+            returned array.
         """
         t = self.measurement[:, 0]
         n = self.measurement.shape[0]
@@ -132,23 +149,23 @@ class GOF_Scores:
             raise ValueError('fmax must be larger than fmin.')
 
         scores = np.array([])
-        if score_groups[0]:
+        if score_arias:
             scores0 = gof.d_1234(self.measurement,self.simulation,fmin,fmax,show_fig)
             for ind, sc in enumerate(scores0):
                 scores = np.append(scores, sc)
                 self.scores[0+ind] = sc
-        if score_groups[1]:
+        if score_rms:
             scores1 = gof.d_567(self.measurement,self.simulation,fmin,fmax,show_fig)
             for ind, sc in enumerate(scores1):
                 scores = np.append(scores, sc)
                 self.scores[4+ind] = sc
-        if score_groups[2]:
+        if score_spectra:
             scores2 = gof.d_89(self.measurement, self.simulation,fmin,fmax,show_fig)
             for ind, sc in enumerate(scores2):
                 scores = np.append(scores, sc)
                 self.scores[7+ind] = sc
-        if score_groups[3]:
-            scores3 = gof.d_10() 
+        if score_cross_correl:
+            scores3 = gof.d_10() # TODO: GOF.D_10() FUNCTION
             scores = np.append(scores, scores3)
             self.scores[9] = scores3
         
@@ -167,53 +184,119 @@ class GOF_Scores:
             print('Goodness of Fit Scores')
             print('---------------------------------------')
             ind = 0
-            if score_groups[0]:
+            if score_arias:
                 [print(f'{sn[ix]:>31}: {s: .3f}') for ix, s in enumerate(scores[ind:ind+4])]
                 ind += 4
-            if score_groups[1]:
+            if score_rms:
                 [print(f'{sn[ix+4]:>31}: {s: .3f}') for ix, s in enumerate(scores[ind:ind+3])]
                 ind += 3
-            if score_groups[2]:
+            if score_spectra:
                 [print(f'{sn[ix+7]:>31}: {s: .3f}') for ix, s in enumerate(scores[ind:ind+2])]
                 ind += 2
-            if score_groups[3]:
+            if score_cross_correl:
                 [print(f'{sn[ix+9]:>31}: {s: .3f}') for ix, s in enumerate(scores[ind])]
             print('---------------------------------------')
             print(f'Average Score: {np.mean(scores):.3f}')
 
         return scores
 
-"""
-### pgain_12.273_vs30_475
-
-Python vs Matlab:
-c1: 0.8587 0.8587
-c2: 0.8004 0.8014
-c3: 1.3485 1.3485 
-c4: 1.1072 1.1072
-c5: 0.6681 0.6681
-c6: 0.5420 0.5420
-c7: 1.0253 0.8836 (updated python: 0.9595)
-c8: 0.9545 1.0016
-c9: -2.2074 -2.5694
-avg score: 0.5664 0.5157
-
-### pgain_15.026_vs30_650
-
-Python vs Matlab:
-c1: 0.9168 0.9168
-c2: 0.9487 0.9487
-c3: -2.6997 -2.6997
-c4: -2.9303 -2.9303
-c5: -1.4648 -1.4648
-c6: -1.6042 -1.6042
-c7: -1.2755 -1.1838 (updated python: -1.2591)
-c8: -1.9881 -2.1110
-c9: -4.2612 -4.5906
-avg score: -1.5954 -1.6354
-"""
-
 class Batch_GOF_Scores:
     """
     Run goodness-of-fit scoring in batch.
     """
+    def __init__(self, list_of_scores: list[GOF_Scores]) -> None:
+        if not isinstance(list_of_scores, list):
+            raise TypeError('`list_of_gof` should be a list.')
+
+        if len(list_of_scores) == 0:
+            raise ValueError(
+                '`list_of_gof` should have at least one element.'
+            )
+
+        n_scores = len(list_of_scores)
+
+        self.list_of_scores = list_of_scores
+        self.n_scores = n_scores
+
+    def run(
+            self,
+            parallel: bool = False,
+            n_cores: int | None = 1,
+            options: dict[str, Any] | None = None,
+    ) -> list[GOF_Scores]:
+        """
+        Run gof scoring in batch.
+
+        Parameters
+        ----------
+        parallel : bool
+            Whether to use multiple CPU cores to run simulations.
+        n_cores : int | None
+            Number of CPU cores to be used. If ``None``, all CPU cores will be
+            used.
+        base_output_dir : str | None
+            The parent directory for saving the output files/figures of the
+            current batch.
+        options : dict[str, Any] | None
+            Options to be passed to the ``run()`` methods of the relevant
+            simulation classes (linear, equivalent linear, or nonlinear). Check
+            out the API documentation of the ``run()`` methods here:
+            https://pyseismosoil.readthedocs.io/en/stable/api_docs/class_simulation.html
+            If None, it is equivalent to an empty dict.
+
+        Returns
+        -------
+        
+        """
+        options = {} if options is None else options
+
+        N = self.n_scores
+
+        score_results = []
+        if not parallel:
+            for i in range(self.n_scores):
+                score_results.append(self._run_single_score([i, options]))
+            # END FOR
+        else:
+            # Because the user may wish to print batch-related verbosity
+            # but it is unideal for the parallel processes to be printing
+            # their progress simultaneously
+            print('Parallel computing in progress...', end=' ')
+                
+            p = mp.Pool(n_cores)
+            score_results = p.map(
+                self._run_single_score,
+                itertools.product(range(N), [options]),
+            )
+
+            print('done.')   
+        # END IF
+            
+        return score_results
+
+    def _run_single_score(self, all_params: list[Any]) -> GOF_Scores:
+        """
+        Run a single simulation.
+
+        Parameters
+        ----------
+        all_params : list[Any]
+            All the parameters needed for running the simulation. It should
+            have the following structure:
+                [i, [catch_errors, options]]
+            where:
+                - ``i`` is the index of the current simulation in the batch.
+                - ``catch_errors``: same as in the ``run()`` method
+                - ``options``: same as in the ``run()`` method
+
+        Returns
+        -------
+        score_result : GOF_Scores
+            Score results for a single GOF score object.
+        """
+        i, options = all_params  # unpack
+
+        score_obj = self.list_of_scores[i]
+        _ = score_obj.calc_scores(**options)
+
+        return score_obj
